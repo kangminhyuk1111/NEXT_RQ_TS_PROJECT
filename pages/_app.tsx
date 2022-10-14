7b5a03e0148c6a0201c5c2f78d668be0fc774cd7
강민혁 <rkdglqkr12@naver.com>
2022-10-15 00:50:34 +0900

    20221015-dabohome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  const client = new QueryClient()
+  return (
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+  )
 }
-
-export default MyApp
